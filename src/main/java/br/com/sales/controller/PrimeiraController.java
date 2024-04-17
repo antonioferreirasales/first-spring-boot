06286af6d4aaeff6bd9a7e33a1acae45f7ac5ca0
Antônio Sales <antoniosales1003@gmail.com>
2024-04-17 16:50:33 -0300

    feat: add response entity

--- src/main/java/br/com/sales/controller/PrimeiraController.java
+++ src/main/java/br/com/sales/controller/PrimeiraController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 @RequestMapping("/primeira")
@@ -35,9 +38,31 @@ public class PrimeiraController {
   record Usuario(String username) {
   }
 
+  // recebe um body no método POST
   @PostMapping("/metodoBodyParams")
   public String metodoBodyParams(@RequestBody Usuario usuario) {
     return "Este é um método POST: " + usuario.username;
+  }
+
+  // recebe um header
+  @PostMapping("/metodoHeaders")
+  public String metodoHeaders(@RequestHeader("name") String name) {
+    return "Este é um método POST: " + name;
+  }
+
+  // recebe e mapeia vários headers
+  @PostMapping("/metodoManyHeaders")
+  public String ManyHeaders(@RequestHeader Map<String, String> headers) {
+    return "Este é um método POST: " + headers.entrySet();
+  }
+
+  @GetMapping("/responseEntity/{id}")
+  public ResponseEntity<Object> metodoResponseEntity(@PathVariable Long id) {
+    var usuario = new Usuario("Rita");
+    if (id > 5) {
+      return ResponseEntity.status(HttpStatus.OK).body(usuario);
+    }
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Mensagem de erro");
   }
 
 }

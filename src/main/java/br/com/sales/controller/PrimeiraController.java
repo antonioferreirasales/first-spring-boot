344498d22d4fcf0892b5633087355395b45b17f6
Antônio Sales <antoniosales1003@gmail.com>
2024-04-17 16:25:28 -0300

    feat: add post method

--- src/main/java/br/com/sales/controller/PrimeiraController.java
+++ src/main/java/br/com/sales/controller/PrimeiraController.java
@@ -8,23 +8,36 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/primeira")
 public class PrimeiraController {
+  // recebe uma requisição com path params
   @GetMapping("/metodo/{id}")
   public String getMethod(@PathVariable String id) {
     return new String("O parâmetro é " + id);
   }
 
+  // define um query params para receber
   @GetMapping("/metodoQuery")
   public String getMethodQuery(@RequestParam String id) {
     return new String("O parâmetro com Query Params é " + id);
   }
 
+  // recebe vários query params e os mapeia
   @GetMapping("/multiQuery")
   public String getMultiQuery(@RequestParam Map<String, String> allParams) {
     return new String("O parâmetro com Query Params é " + allParams.entrySet());
+  }
+
+  record Usuario(String username) {
+  }
+
+  @PostMapping("/metodoBodyParams")
+  public String metodoBodyParams(@RequestBody Usuario usuario) {
+    return "Este é um método POST: " + usuario.username;
   }
 
 }
